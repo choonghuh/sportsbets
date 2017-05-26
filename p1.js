876d35e80b88a96e404733964178d02254f67858
Choong Huh <choonghuh@gmail.com>
2017-05-25 21:08:28 -0700

    Enough headaches for today... Inserting into DB in an awful way

--- p1.js
+++ p1.js
@@ -6,6 +6,33 @@ var sqlite3 = require('sqlite3')
 var dbFileName = 'tempdb';
 var db = new sqlite3.Database(dbFileName);
 
+var dbTextSource = 'static/bets_source';
+var betArray = [];
+
+function buildFromTextFile(sourcefile){
+	console.log('buildFromTextFile');
+	fs.readFile(sourcefile ,function(err, data){
+		var betStrings = data.toString().split('\n');
+		for(i in betStrings){
+			temp = betStrings[i].split(',');
+			betArray.push([
+				temp[0],
+				temp[1],
+				temp.slice(2,temp.length).join() //In case when the string has a comma...
+			]);
+		}
+		console.log(betArray);
+		for (i in betArray){
+			console.log('INSERT INTO bets VALUES (NULL, ' + betArray[i].join(', ') +' )');
+			db.run('INSERT INTO bets VALUES (NULL, ' + betArray[i].join(', ') +' )');
+		}
+		db.each("SELECT * FROM bets", function(err, row) {
+			console.log(row);
+		});
+
+		db.close();
+	});
+}
 db.serialize(function() {
 	var createBetsTableQuery = 'CREATE TABLE bets (';
 	// Add columns to table - COLUMN DATATYPE ETC,
@@ -18,13 +45,16 @@ db.serialize(function() {
 	var insertBetQuery = 'INSERT INTO bets VALUES ';
 	db.run(insertBetQuery + '(NULL, 5, "I win", "betking")');
 
-	db.each("SELECT * FROM bets", function(err, row) {
-		console.log(row);
-	});
+	console.log("Initial DB Population");
+	buildFromTextFile(dbTextSource);
+
+	// db.each("SELECT * FROM bets", function(err, row) {
+	// 	console.log(row);
+	// });
 
 });
 
-db.close();
+// db.close();
 
 // then create a server
 // http.createServer(function(req, res){
